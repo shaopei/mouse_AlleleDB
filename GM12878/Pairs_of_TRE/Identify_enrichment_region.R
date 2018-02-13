69c041a5c26575d9efcf2eeed9f70e556c6afe24
shaopei <sc2457@cornell.edu>
2018-02-13 15:12:59 -0500

    script to identify the size of HMM group

--- GM12878/Pairs_of_TRE/Identify_enrichment_region.R
+++ GM12878/Pairs_of_TRE/Identify_enrichment_region.R
@@ -3,12 +3,13 @@ f1p="counts_both_hmm_regions_t1e-05_interestingHets_IGV.bed_distanceToNearestReg
 #f1p="counts_both_hmm_regions_t1e-05_interestingHets_IGV.bed_pairwise_distance.txt"
 f1=read.table(f1p,header=F)
 
-hist(f1$V1, col="blue",
-     breaks = seq(0,5000000,25000), freq = FALSE,
+Uplimit=5e06
+hist(f1$V1[f1$V1<= Uplimit], col="blue",
+     breaks = seq(0,Uplimit,25000), freq = FALSE,
      xlab="distance(bp)",
-     main="distanceToNearestRegion") 
+     main="distance to nearest region") 
 
-Uplimit=100000
+Uplimit=1e05
 hist(f1$V1[f1$V1<=Uplimit], col="blue", xlim=c(0,Uplimit),
      breaks = seq(0,Uplimit,2500), freq = FALSE,
      xlab="distance(bp)",
@@ -16,6 +17,7 @@ hist(f1$V1[f1$V1<=Uplimit], col="blue", xlim=c(0,Uplimit),
 
 # use MLE 
 lamda = with(f1, 1/mean(V1))
+x=seq(0,Uplimit,Uplimit/5000)
 lines(x, lamda*exp(-lamda*x), col="orange",lwd=1.5)
 legend("topright", 
        legend = paste("Exponential pdf lamda = ", format(lamda, digits=2, nsmall=2),sep=""),
