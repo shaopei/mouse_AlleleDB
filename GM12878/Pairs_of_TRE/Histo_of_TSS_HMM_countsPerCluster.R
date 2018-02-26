e73c7b98530ec96c9845c33395bd19cc3f6feda1
shaopei <sc2457@cornell.edu>
2018-02-26 16:41:49 -0500

    update

--- GM12878/Pairs_of_TRE/Histo_of_TSS_HMM_countsPerCluster.R
+++ GM12878/Pairs_of_TRE/Histo_of_TSS_HMM_countsPerCluster.R
@@ -1,49 +1,16 @@
 setwd("~/Box Sync/Danko_lab_work/mouse_AlleleDB/GM12878/Pairs_of_TRE/")
 setwd("~/Box Sync/Danko_lab_work/mouse_AlleleDB/GM12878/Pairs_of_TRE/B6_CAST/")
-f1p="counts_both_hmm_regions_t1e-05_interestingHets_IGV.bed_distanceToNearestRegion.txt"
-#f1p="counts_both_hmm_regions_t1e-05_interestingHets_IGV.bed_distanceToNearestRegion_excludeChr6.txt"
-
-#f1p="counts_both_hmm_regions_t1e-05_interestingHets_IGV.bed_pairwise_distance.txt"
-f1=read.table(f1p,header=F)
-Uplimit=5e06
-hist(f1$V1, col="blue",
-     breaks = seq(0,Uplimit,25000), 
-     freq = FALSE,
-     xlab="distance(bp)",
-     main="distance to nearest region") 
-Uplimit=1e05
-Uplimit=1000000
-hist(f1$V1[f1$V1<= Uplimit], col="blue",
-     breaks = seq(0,Uplimit,5000), 
-     freq = FALSE,
-     xlab="distance(bp)",
-     main="distance to nearest region") 
-
-Uplimit=20000
-hist(f1$V1[f1$V1<=Uplimit], col="blue", xlim=c(0,Uplimit),
-     breaks = seq(0,Uplimit,2500), freq = FALSE,
-     xlab="distance(bp)",
-     main="distanceToNearestRegion")
-
-# use MLE 
-lamda = with(f1, 1/mean(V1))
-x=seq(0,Uplimit,Uplimit/50000)
-lines(x, lamda*exp(-lamda*x), col="orange",lwd=1.5)
-legend("topright", 
-       legend = paste("Exponential pdf lamda = ", format(lamda, digits=2, nsmall=2),sep=""),
-       lty=1, lwd=1.5, col="orange", bty = "n")
-
 
 f2p="20000_cluster_region_count.txt"
 f2p="counts_both_hmm_regions_t1e-05_interestingHets_IGV_clusterBy10000bp_region_count.txt"
 f2=read.table(f2p,header=F)
 hist(f2$V1,col=mycol1
-     , breaks = seq(0,30,1)
+     , breaks = seq(0,200,1)
      , freq = F
      #, xlab="HMM blocks in the cluster"
      #, add=T
-     , xlim=c(0,35)
-     )
+     , xlim=c(0,50)
+)
 lamda = with(f2, 1/mean(V1))
 x=seq(0,200,1)
 lines(x, lamda*exp(-lamda*x), col="dark orange",lwd=1.5)
@@ -52,12 +19,12 @@ legend("topright",
        legend = paste("Exponential pdf lamda = ", format(lamda, digits=2, nsmall=2),sep=""),
        lty=1, lwd=1.5, col="orange", bty = "n")
 
-f3p="tss_all_gm12878_clusterBy20000bp_count.txt"
+f3p="counts_both_hmm_regions_t1e-05_interestingHets_IGV_clusterBy20000bp_tssPerClusterCount.txt" 
 f3p="counts_both_hmm_regions_t1e-05_interestingHets_IGV_clusterBy10000bp_dRegPerClusterCount.txt"
 f3=read.table(f3p,header=F)
 #hist(f3$V1, freq = F, col=mycol2)
 hist(f3$V1, freq = F
-     , breaks = seq(0,35,1)
+     , breaks = seq(0,200,1)
      , add=T
      , xlab="Numer of TSSs in the cluster"
      , col=mycol2)
@@ -88,36 +55,8 @@ t_col <- function(color, percent = 50, name = NULL) {
                names = name)
   ## Save the color
   invisible(t.col)
-  }
+}
 
 mycol1 <- t_col("blue", perc = 50, name = "lt.blue")
 mycol2 <- t_col("green", perc = 50, name = "lt.green")
 ## END
-
-
-
-
-### use fitdistr
-dis=f1$V1
-fit1 <- fitdistr(dis, "geometric") 
-fit2 <- fitdistr(dis, "exponential") 
-hist(dis, freq = FALSE, breaks = 5000, xlim = c(0, 200000))
-curve(dexp(x, rate = fit1$estimate), col = "red", add = TRUE)
-curve(dexp(x, rate = fit2$estimate), col = "blue", add = TRUE)
-curve(dexp(x, rate = lamda), col = "orange", add = TRUE)
-
-## use lm 
-Uplimit=20000
-breaks=5000
-dummy=1
-a=with (f1, hist(V1, col="blue", 
-                 xlab='distance (base)',
-                 breaks = breaks))
-
-count=a$counts+dummy
-dis=a$mids
-exponential.model = lm(log(count) ~ dis)
-summary(exponential.model)
-x=seq(0,max(dis),max(dis)/5000)
-y=exp(coef(exponential.model)[1]+x*coef(exponential.model)[2])
-lines(x, y, col="red")
--- GM12878/Pairs_of_TRE/correlation_of_regions_in_a_cluster.R
+++ GM12878/Pairs_of_TRE/correlation_of_regions_in_a_cluster.R
@@ -11,16 +11,40 @@ hist(unlist(lapply(1:max(f1$V7), function(i){sum(f1$V7==i)})), breaks=seq(1,200,
 dummy=1
 f1$clusterSize=unlist(lapply(f1$V7, function(i){sum(f1$V7==i)}))
 f1$winP=unlist(lapply(f1$V4, function(i){strsplit(as.character(i),',',fixed=TRUE)[[1]][1]}))
-f1$matCount=unlist(lapply(f1$V4, function(i){as.numeric(strsplit(as.character(i),',',fixed=TRUE)[[1]][2])}))
-f1$patCount=unlist(lapply(f1$V4, function(i){as.numeric(strsplit(as.character(i),',',fixed=TRUE)[[1]][3])}))
-f1$mat_pat_log2ratio=log2((f1$matCount+dummy)/(f1$patCount+dummy))
+# matCount is matReadsCount
+f1$matReadsCount=unlist(lapply(f1$V4, function(i){as.numeric(strsplit(as.character(i),',',fixed=TRUE)[[1]][2])}))
+f1$patReadsCount=unlist(lapply(f1$V4, function(i){as.numeric(strsplit(as.character(i),',',fixed=TRUE)[[1]][3])}))
+f1$mat_pat_ReadsCount_log2ratio=log2((f1$matReadsCount+dummy)/(f1$patReadsCount+dummy))
 
+# the start and end of each cluster
+for (i in 1:max(f1$V7)){
+  f1$minStart[f1$V7==i]<- min(f1$V2[f1$V7==i])
+  f1$maxEnd[f1$V7==i]<- max(f1$V3[f1$V7==i])
+}
+
+# keep cluster with more than one HMM blocks
+cluster_moreThan1=f1[f1$clusterSize>1,c(1,13,14,8)] 
+cluster_moreThan1 = cluster_moreThan1[!duplicated(cluster_moreThan1),]
+View(cluster_moreThan1)
+write.table(cluster_moreThan1, paste(f1p,"_cluster_moreThan1.bed",sep="")
+            , sep = "\t",row.names = F,quote = F,
+            col.names = F)
+
+# study correlations below
 cluster2=f1[f1$clusterSize==2,]  
 View(cluster2)
 
-pair1=unlist(lapply(seq(1,dim(cluster2)[1],2), function(i){cluster2$mat_pat_log2ratio[i]}))
-pair2=unlist(lapply(seq(2,dim(cluster2)[1],2), function(i){cluster2$mat_pat_log2ratio[i]}))
-plot(pair1,pair2)
+pair1_s=unlist(lapply(seq(1,dim(cluster2)[1],2), function(i){cluster2$V6[i]}))
+pair2_s=unlist(lapply(seq(2,dim(cluster2)[1],2), function(i){cluster2$V6[i]}))
+sum(pair1_s==pair2_s)
+sum(pair1_s!=pair2_s)
+pair1=unlist(lapply(seq(1,dim(cluster2)[1],2), function(i){cluster2$mat_pat_ReadsCount_log2ratio[i]}))
+pair2=unlist(lapply(seq(2,dim(cluster2)[1],2), function(i){cluster2$mat_pat_ReadsCount_log2ratio[i]}))
+plot(pair1[pair1_s!=pair2_s],pair2[pair1_s!=pair2_s]
+     , xlab="mat_pat_log2ratio"
+     , ylab="mat_pat_log2ratio"
+     , col="orange")
+points(pair1[pair1_s==pair2_s],pair2[pair1_s==pair2_s])
 abline(h=0)
 abline(v=0)
 
@@ -30,3 +54,30 @@ sum(pair1==pair2)
 sum(pair1!=pair2)
 length(pair1)
 
+###
+cluster3=f1[f1$clusterSize==3,]  
+View(cluster3)
+tri1s=unlist(lapply(seq(1,dim(cluster3)[1],3), function(i){cluster3$V6[i]}))
+tri2s=unlist(lapply(seq(2,dim(cluster3)[1],3), function(i){cluster3$V6[i]}))
+tri3s=unlist(lapply(seq(3,dim(cluster3)[1],3), function(i){cluster3$V6[i]}))
+
+tri1=unlist(lapply(seq(1,dim(cluster3)[1],3), function(i){cluster3$mat_pat_ReadsCount_log2ratio[i]}))
+tri2=unlist(lapply(seq(2,dim(cluster3)[1],3), function(i){cluster3$mat_pat_ReadsCount_log2ratio[i]}))
+tri3=unlist(lapply(seq(3,dim(cluster3)[1],3), function(i){cluster3$mat_pat_ReadsCount_log2ratio[i]}))
+plot(tri1[tri1s!=tri2s],tri2[tri1s!=tri2s]
+     , xlab="mat_pat_log2ratio"
+     , ylab="mat_pat_log2ratio"
+     , col="orange")
+points(tri1[tri1s!=tri3s],tri3[tri1s!=tri3s]
+       , col="orange")
+points(tri2[tri2s!=tri3s],tri3[tri2s!=tri3s]
+       , col="orange")
+points(tri1[tri1s==tri2s],tri2[tri1s==tri2s])
+points(tri1[tri1s==tri3s],tri3[tri1s==tri3s])
+points(tri2[tri2s==tri3s],tri3[tri2s==tri3s])
+length(c(tri1[tri1s!=tri2s], tri2[tri2s!=tri3s], tri1[tri1s!=tri3s]))
+length(c(tri1[tri1s==tri2s], tri2[tri2s==tri3s], tri1[tri1s==tri3s]))
+
+sum(tri1[tri1s==tri2s]*tri2[tri1s==tri2s] <0)+
+sum(tri1[tri1s==tri3s]*tri3[tri1s==tri3s] <0)+
+sum(tri2[tri2s==tri3s]*tri3[tri2s==tri3s] <0)
