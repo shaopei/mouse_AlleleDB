14b4df0fdab975bc3c6a0897110fcb3de29c4681
shaopei <sc2457@cornell.edu>
2018-10-30 10:06:18 -0400

    update for script moving

--- GM12878/SeqDepth_AlleleHMM_performance_test/02_AlleleDB_Run.bash
+++ GM12878/SeqDepth_AlleleHMM_performance_test/02_AlleleDB_Run.bash
@@ -1,4 +1,13 @@
-ln -s /workdir/sc2457/GM_GroSeq_AlleleDB_20170606/test_in_total/alleledb_strandSpecific_9.sh .
+#cd /workdir/sc2457/SeqDepth_AlleleHMM_performance_test
+#mv /workdir/sc2457/GM_GroSeq_AlleleDB_20170606/test_in_total/alleledb_strandSpecific_9.sh .
+#cd /workdir/sc2457/GM_GroSeq_AlleleDB_20170606/test_in_total/
+#ln -s /workdir/sc2457/SeqDepth_AlleleHMM_performance_test/alleledb_strandSpecific_9.sh .
+
+#cd /workdir/sc2457/SeqDepth_AlleleHMM_performance_test
+#mv /workdir/sc2457/GM_GroSeq_AlleleDB_20170606/test_in_total/PIPELINE_StrandSpecific.mk .
+#cd /workdir/sc2457/GM_GroSeq_AlleleDB_20170606/test_in_total/
+#ln -s /workdir/sc2457/SeqDepth_AlleleHMM_performance_test/PIPELINE_StrandSpecific.mk .
+
 for file in SRR1552485_*.fastq.gz
 do 
 f=`echo ${file} | rev | cut -d . -f 3 |rev`
@@ -17,7 +26,7 @@ NA12878_hg19_150109_w_transcriptome \
 /workdir/sc2457/SeqDepth_AlleleHMM_performance_test/${f} \
 ${f} \
 /workdir/sc2457/alleleDB/alleledb_pipeline \
-/workdir/sc2457/GM_GroSeq_AlleleDB_20170606/test_in_total/PIPELINE_StrandSpecific.mk \
+/workdir/sc2457/SeqDepth_AlleleHMM_performance_test/PIPELINE_StrandSpecific.mk \
 0.1 \
 ase \
 0 > ${f}_alleledb_strandSpecific_9.log  2>&1 &
